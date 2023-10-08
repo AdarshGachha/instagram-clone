1fde392dd06901f6e43337fab6df7d1fd32d07ea
iammradarsh <gachhaadarsh122@gmail.com>
2023-10-08 17:04:53 +0530

    changes index route

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/instagram');
+// mongoose.connect('mongodb://127.0.0.1:27017/instagram');
 
 var postsSchema = mongoose.Schema({
   image: String,
